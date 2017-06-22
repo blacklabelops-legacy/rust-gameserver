8b1b7d63eee2f03b442a1b2ad0336db4f0ecdba6
Steffen Bleul <sbl@itbleul.de>
2017-06-22 17:18:39 +0200

    Seed override mechanism over file. For random server feeds in automated wipes.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,13 @@
 
 set -o errexit
 
+if [ -n "${RUST_SEED_FILE}" ]; then
+  if [ -e "${RUST_SEED_FILE}" ]; then
+	   RUST_SERVER_SEED=`cat ${RUST_SEED_FILE}`
+     echo "New server seed: "${RUST_SERVER_SEED}
+  fi
+fi
+
 if [ "$1" = 'rust' ]; then
   exec /start.sh
 else
