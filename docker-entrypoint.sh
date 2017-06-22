0cf3195632e09d275636d4b2d542627371576278
Steffen Bleul <sbl@itbleul.de>
2017-06-22 17:37:58 +0200

    Better label for seed override.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@ set -o errexit
 if [ -n "${RUST_SEED_FILE}" ]; then
   if [ -e "${RUST_SEED_FILE}" ]; then
 	   RUST_SERVER_SEED=`cat ${RUST_SEED_FILE}`
-     echo "New server seed: "${RUST_SERVER_SEED}
+     echo "Server Seed Override, Seed: "${RUST_SERVER_SEED}
   fi
 fi
 
